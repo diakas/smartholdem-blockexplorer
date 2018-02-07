76396110751c4e834a49839429c30ebcf82434bc
technolog <viktor@allz.su>
2018-02-07 23:30:33 +0300

    FUNDS

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,10 +7,12 @@ export const CONFIG = {
         NETHASH: 'fc46bfaf9379121dd6b09f5014595c7b7bd52a0a6d57c5aff790b42a73c76da7',
         CURRENCIES: ['COINS'],
         KNOWN_ADDRESSES: {
-          "SVx2j3NdZbDLfZ9HWS57NvqYg4c9grQLnx" : "Bounty",
-          "SUeGCt31AHwTZVcfZQwpPVL4jEUCtMMDTg" : "Dev fund",
-          "SU32qNJaBFGBgWhzkhwfGN2nExXYhtDCXU" : "Tournament",
-          "Sa9ikuhxMz6nWzAyqriiXSptgdwo2BwumP" : "PostCoin Community"
+          "SVx2j3NdZbDLfZ9HWS57NvqYg4c9grQLnx" : "BOUNTY",
+          "SUeGCt31AHwTZVcfZQwpPVL4jEUCtMMDTg" : "DEV FUND [frozen]",
+          "SU32qNJaBFGBgWhzkhwfGN2nExXYhtDCXU" : "TOURNAMENT FUND",
+          "SZjrWHZncQpJjvHzGqrnMWcm5yCgYhMMvB" : "NODES FUND",
+          "Sa9ikuhxMz6nWzAyqriiXSptgdwo2BwumP" : "POSTCOIN NODE",
+          "STkW1XN3LGn8R3FStMpboNXZugDb6Aq67E" : "EXCHANGE FUND",
         },
         PROPERTIES: [
           'DISABLE_PRICE_API',
